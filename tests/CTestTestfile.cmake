0908f1af98acc39a3565f26a4fba5430d1adfdb9
Mateusz Marek Wojtyś <mateo.wojtys@gmail.com>
2025-07-18 08:52:56 +0200

    Clean-up

--- tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mttw/Desktop/dev/osiris_chaind/project/osiris-core/tests
-# Build directory: /home/mttw/Desktop/dev/osiris_chaind/project/osiris-core/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/mttw/Desktop/dev/osiris_chaind/project/osiris-core/tests/osiris-tests[1]_include.cmake")
-subdirs("../_deps/googletest-build")
